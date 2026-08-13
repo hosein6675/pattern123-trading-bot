714471f86fc9251f6b890e171564d7e496287671
hosein6675 <hoseinrezaei2148@gmail.com>
2026-08-13 14:05:34 +0330

    Update bot_controller.py

--- modules/bot_controller.py
+++ modules/bot_controller.py
@@ -1,1 +1,27 @@
+from modules.trading_engine import TradingEngine
+from modules.config import active_config
 
+
+class BotController:
+
+    def __init__(self):
+        self.engine = TradingEngine()
+
+
+    def get_status(self):
+
+        return {
+            "market": active_config.market,
+            "symbol": active_config.symbol,
+            "timeframe": active_config.timeframe,
+            "mode": active_config.mode
+        }
+
+
+    def analyze(self, candles):
+
+        return self.engine.analyze_market(
+            active_config.symbol,
+            active_config.timeframe,
+            candles
+        )
