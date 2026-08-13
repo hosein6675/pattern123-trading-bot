b1be9420a93339eb738952318cdc7d533d254ea7
hosein6675 <hoseinrezaei2148@gmail.com>
2026-08-13 19:50:05 +0330

    Update order_manager.py

--- modules/order_manager.py
+++ modules/order_manager.py
@@ -1,1 +1,59 @@
+from modules.broker_interface import BrokerInterface
 
+
+class OrderManager:
+
+
+    def __init__(self):
+
+        self.broker = BrokerInterface()
+
+
+
+    def execute_trade(
+        self,
+        symbol,
+        direction,
+        volume,
+        stop_loss,
+        take_profit
+    ):
+
+
+        order = self.broker.open_order(
+
+            symbol=symbol,
+
+            direction=direction,
+
+            volume=volume,
+
+            stop_loss=stop_loss,
+
+            take_profit=take_profit
+
+        )
+
+
+        return order
+
+
+
+    def close_trade(self, order_id):
+
+
+        result = self.broker.close_order(
+
+            order_id
+
+        )
+
+
+        return result
+
+
+
+    def get_open_positions(self):
+
+
+        return self.broker.get_positions()
