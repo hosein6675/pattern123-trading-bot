e95002ca3d93a150bd3dccd4fa49b884b640fd84
hosein6675 <hoseinrezaei2148@gmail.com>
2026-08-13 18:10:16 +0330

    Update account_manager.py

--- modules/account_manager.py
+++ modules/account_manager.py
@@ -1,1 +1,44 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class AccountInfo:
+
+    balance: float
+    equity: float
+    mode: str
+
+
+
+class AccountManager:
+
+
+    def __init__(self):
+
+        # حساب دمو اولیه
+        self.balance = 1000.0
+        self.equity = 1000.0
+        self.mode = "demo"
+
+
+
+    def get_account(self):
+
+        return AccountInfo(
+
+            balance=self.balance,
+
+            equity=self.equity,
+
+            mode=self.mode
+
+        )
+
+
+
+    def update_balance(self, new_balance):
+
+        self.balance = new_balance
+
+
+        self.equity = new_balance
