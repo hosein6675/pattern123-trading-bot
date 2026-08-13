40560b220d59ae2f4be27f04a1650a0b026f5822
hosein6675 <hoseinrezaei2148@gmail.com>
2026-08-13 18:15:48 +0330

    Update trading_engine.py

--- modules/trading_engine.py
+++ modules/trading_engine.py
@@ -3,41 +3,82 @@ from modules.price_action import PriceActionEngine
 from modules.macd_engine import MACDEngine
 from modules.risk_manager import RiskManager
 from modules.journal import JournalEngine
+from modules.account_manager import AccountManager
+
 
 
 class TradingEngine:
 
+
     def __init__(self):
 
         self.structure = StructureAnalyzer()
+
         self.price_action = PriceActionEngine()
+
         self.macd = MACDEngine()
+
         self.risk = RiskManager()
+
         self.journal = JournalEngine()
+
+        self.account = AccountManager()
+
 
 
     def analyze_market(self, symbol, timeframe, candles):
 
+
+        # دریافت وضعیت حساب دمو
+        account = self.account.get_account()
+
+
+
+        # تحلیل ساختار بازار
         structure_result = self.structure.analyze(candles)
 
+
+
+        # تحلیل پرایس اکشن
         pa_result = self.price_action.analyze(
             structure_result,
             candles
         )
 
+
+
+        # تحلیل MACD
         macd_result = self.macd.analyze(candles)
 
+
+
+        # بررسی مدیریت سرمایه
         risk_result = self.risk.check(
-            0,
-            0,
-            0
+
+            balance=account.balance,
+
+            loss_amount=0,
+
+            position_size=0
+
         )
 
+
+
         return {
+
             "symbol": symbol,
+
             "timeframe": timeframe,
+
+            "account": account,
+
             "structure": structure_result,
+
             "price_action": pa_result,
+
             "macd": macd_result,
+
             "risk": risk_result
+
         }
