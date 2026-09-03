d3d32bbf6bd779f8d66d77e6e81b700a690bfa6f
hosein6675 <hoseinrezaei2148@gmail.com>
2026-09-03 15:25:23 +0330

    feat: expose broker account and execution capabilities

--- modules/order_manager.py
+++ modules/order_manager.py
@@ -2,13 +2,25 @@ from modules.broker_interface import BrokerInterface
 
 
 class OrderManager:
+    def __init__(self, broker: BrokerInterface | None = None):
+        self.broker = broker or BrokerInterface()
 
+    def connect(self):
+        return self.broker.connect()
 
-    def __init__(self):
+    def disconnect(self):
+        self.broker.disconnect()
 
-        self.broker = BrokerInterface()
+    def status(self):
+        return self.broker.status()
 
+    def account_info(self):
+        return self.broker.account_info()
 
+    def risk_per_lot(self, symbol, direction, entry, stop_loss):
+        return self.broker.risk_per_lot(
+            symbol, direction, entry, stop_loss
+        )
 
     def execute_trade(
         self,
@@ -16,44 +28,18 @@ class OrderManager:
         direction,
         volume,
         stop_loss,
-        take_profit
+        take_profit,
     ):
-
-
-        order = self.broker.open_order(
-
+        return self.broker.open_order(
             symbol=symbol,
-
             direction=direction,
-
             volume=volume,
-
             stop_loss=stop_loss,
-
-            take_profit=take_profit
-
+            take_profit=take_profit,
         )
-
-
-        return order
-
-
 
     def close_trade(self, order_id):
-
-
-        result = self.broker.close_order(
-
-            order_id
-
-        )
-
-
-        return result
-
-
+        return self.broker.close_order(order_id)
 
     def get_open_positions(self):
-
-
         return self.broker.get_positions()
